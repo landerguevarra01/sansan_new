aafe4e3ff3fd70869d53dfa65e18329ef0f37cee
Lander <guevarralander0@gmail.com>
2024-11-11 23:40:34 +0800

    prefer layout3

--- src/HeroBanner/Hero.js
+++ src/HeroBanner/Hero.js
@@ -3,18 +3,18 @@ import JaysonIcon from "../Assets/jaysonicon.png";
 function Hero() {
   return (
     <>
-      <div className="flex flex-col justify-center item-center text-center text-responsiveBanner font-bold mt-40">
+      <div className="flex flex-col justify-center item-center text-center text-responsiveBanner font-bold mt-40 -tracking-wider">
         <div className="flex flex-col justify-center items-center">
           <img src={JaysonIcon} alt="Jayson Icon" className="" />
         </div>
         <div>HEY THERE! I’M SANSAN, A</div>
         <div className="flex flex-col justify-center items-center">
-          <button className="w-fit flex justify-center items-center border-[#DB0102] border-solid bg-[#DB0102] rounded-full">
+          <button className="w-fit flex justify-center items-center gap-4 border-[#DB0102] border-solid bg-[#DB0102] text-[#1D1D1D] rounded-full px-10">
             GRAPHIC DESIGNER
             <div className="-mt-8">
               <svg
-                width="42"
-                height="42"
+                width="32"
+                height="32"
                 viewBox="0 0 42 42"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -31,28 +31,30 @@ function Hero() {
         </div>
         <div className="">Welcome to my creative space!</div>
         <div className="flex flex-row justify-center items-center">
-          <div className="">GOT A PROJECT IN MIND?</div>
-          <div>
-            <button className="w-fit flex flex-row justify-center items-center border-[#B0B0B0] border-solid bg-[#B0B0B0] rounded-full">
-              LET’S CHAT
-              <div className="-mt-8">
-                <svg
-                  width="42"
-                  height="42"
-                  viewBox="0 0 42 42"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M6 36L38 4M38 4H6M38 4V36"
-                    stroke="#1E1E1E"
-                    stroke-width="8"
-                    stroke-linecap="square"
-                  />
-                </svg>
-              </div>
-            </button>
-          </div>
+          GOT A PROJECT IN MIND?
+          <span>
+            <div>
+              <button className="w-fit flex flex-row justify-center items-center gap-4 border-[#B0B0B0] border-solid bg-[#B0B0B0] text-[#1D1D1D] rounded-full px-10 ml-4">
+                LET’S CHAT
+                <div className="-mt-8">
+                  <svg
+                    width="32"
+                    height="32"
+                    viewBox="0 0 42 42"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6 36L38 4M38 4H6M38 4V36"
+                      stroke="#1E1E1E"
+                      stroke-width="8"
+                      stroke-linecap="square"
+                    />
+                  </svg>
+                </div>
+              </button>
+            </div>
+          </span>
         </div>
       </div>
     </>
